8c5812836946d4b3684ebf610b7dfba9d17b5226
Daniele Megna <daniele.megna@casavo.com>
2022-10-29 12:32:18 +0200

    First try of artist slide from left and from right

--- metrica.js
+++ metrica.js
@@ -2,16 +2,22 @@ $(document).ready(() => {
 
   $(window).bind("scroll", () => {
 
-    const artist = $(".artist-left-slide")
-    const container = artist.parent()
-
     const pageBottom = $(window).scrollTop() + $(window).height()
-    const elementTop = $(container).offset().top
-    const delta = pageBottom - elementTop
-    const newMargin = -artist.width() - 200 + delta
 
-    if (newMargin < 0) {
-      artist.css("margin-left", newMargin)
+    const artist = $(".artist-left-slide")
+    const artistDelta = pageBottom - $(artist).offset().top
+    const newLeftMargin = -artist.height() - 200 + artistDelta
+
+    if (newLeftMargin < 0) {
+      artist.css("margin-left", newLeftMargin)
+    }
+
+    const logo = $(".logo-right-slide")
+    const logoDelta = pageBottom - $(logo).offset().top
+    const newRightMargin = -logo.height() - 400 + logoDelta
+
+    if (newRightMargin < 0) {
+      logo.css("margin-right", newRightMargin)
     }
 
 

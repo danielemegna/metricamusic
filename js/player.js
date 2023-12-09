ae6c7be002699a2f8ee6b1dd0327d3e2d7d47a54
Daniele Megna <daniele.megna@qmates.tech>
2023-12-09 08:50:52 +0100

    Refactoring in player.js

--- js/player.js
+++ js/player.js
@@ -1,29 +1,56 @@
 const albumTitle = document.querySelector("h3.album-title").innerText;
 const albumCoverSrc = document.querySelector("img.cover").getAttribute("src");
 const albumTracks = Array.from(document.querySelectorAll("span.track[audio]"));
-const audioTag = document.querySelector("audio");
+const audioHtmlTag = document.querySelector("audio");
 const pauseButton = document.querySelector("#pause-button");
+
+// ******* EVENT HANDLERS **********
 
 albumTracks.forEach((track) => {
   track.addEventListener("click", (e) => {
     const trackToPlay = e.currentTarget;
-    albumTracks.forEach((t) => t.classList.remove("playing"));
-    trackToPlay.classList.add("playing");
-    pauseButton.classList.remove("hidden");
     playTrack(trackToPlay);
+    showPauseButton();
   });
 });
 
-//navigator.mediaSession.setActionHandler('previoustrack', () => previousTrack());
-navigator.mediaSession.setActionHandler('nexttrack', () => nextTrack());
-audioTag.addEventListener("ended", () => nextTrack());
-
 navigator.mediaSession.setActionHandler('play', () => audioPlay());
 navigator.mediaSession.setActionHandler('pause', () => audioPause());
+navigator.mediaSession.setActionHandler('nexttrack', () => nextTrack());
+//navigator.mediaSession.setActionHandler('previoustrack', () => previousTrack());
+audioHtmlTag.addEventListener("ended", () => nextTrack());
 pauseButton.addEventListener("click", () => toggleAudioPlayPause());
 
+// ******* END EVENT HANDLERS **********
+
+function playTrack(trackToPlay) {
+  const trackAudioSrc = trackToPlay.getAttribute('audio');
+  audioHtmlTag.setAttribute("src", trackAudioSrc);
+  audioPlay();
+
+  albumTracks.forEach((t) => t.classList.remove("playing"));
+  trackToPlay.classList.add("playing");
+
+  const trackTitle = trackToPlay.querySelector('h3').innerText
+  const trackArtist = trackToPlay.querySelector('h5').innerText
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: trackTitle,
+    artist: trackArtist,
+    album: albumTitle,
+    artwork: [{ src: albumCoverSrc, type: 'image/png' }]
+  });
+}
+
+function nextTrack() {
+  const playingTrack = document.querySelector("span.track.playing");
+  const playingTrackIndex = albumTracks.indexOf(playingTrack);
+  const nextTrackIndex = (playingTrackIndex + 1) % albumTracks.length;
+  const nextTrack = albumTracks[nextTrackIndex];
+  playTrack(nextTrack);
+}
+
 function toggleAudioPlayPause() {
-  if(audioTag.paused) {
+  if(audioHtmlTag.paused) {
     audioPlay();
     return;
   }
@@ -31,43 +58,17 @@ function toggleAudioPlayPause() {
 }
 
 function audioPlay() {
-  audioTag.play();
+  audioHtmlTag.play();
   pauseButton.classList.remove('play');
   pauseButton.classList.add('pause');
 }
 
 function audioPause() {
-  audioTag.pause();
+  audioHtmlTag.pause();
   pauseButton.classList.remove('pause');
   pauseButton.classList.add('play');
 }
 
-function playTrack(track) {
-  const trackAudioSrc = track.getAttribute('audio');
-  const trackTitle = track.querySelector('h3').innerText
-  const trackArtist = track.querySelector('h5').innerText
-
-  navigator.mediaSession.metadata = new MediaMetadata({
-    title: trackTitle,
-    artist: trackArtist,
-    album: albumTitle,
-    artwork: [{ src: albumCoverSrc, type: 'image/png' }]
-  });
-
-  audioTag.setAttribute("src", trackAudioSrc);
-  audioPlay();
-}
-
-function nextTrack() {
-  const playingTrack = document.querySelector("span.track.playing");
-  const playingTrackIndex = albumTracks.indexOf(playingTrack);
-  var nextTrackIndex = playingTrackIndex + 1;
-  if (nextTrackIndex === albumTracks.length)
-    nextTrackIndex = 0;
-
-  const nextTrack = albumTracks[nextTrackIndex];
-
-  playingTrack.classList.remove("playing");
-  nextTrack.classList.add("playing");
-  playTrack(nextTrack);
+function showPauseButton() {
+  pauseButton.classList.remove("hidden");
 }
